73051a4325f4322399f2be0a4340abb52f4fc4a9
deeksha-ra <deekshasanjanar@gmail.com>
2026-09-15 10:10:24 +0530

    Create Java Generics.java

---
+++ Unit 3/Week 7/Java Generics.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
+Your Output (stdout)
+1
+2
+3
+Hello
+World
+Expected Output
+1
+2
+3
+Hello
+World
